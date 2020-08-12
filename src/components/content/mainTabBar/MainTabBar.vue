59e137cb46f07ed217ebfb7918262b874560c0bf
coder-wjh <1434624546@qq.com>
2020-08-12 22:44:54 +0800

    配置

--- src/components/content/mainTabBar/MainTabBar.vue
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -2,23 +2,23 @@
   <TabBar>
       <TabBarItem path='/home' activeColor='deepPink'>
       <!-- 别称引用加 ~ ，import不用加 -->
-        <img src="~assets/img/tabbar/home1.svg" alt="" slot="item-icon">
-        <img src="~assets/img/tabbar/home.svg" alt="" slot="item-icon-active">
+        <img src="../../../assets/img/tabbar/home.svg" alt="" slot="item-icon">
+        <img src="~assets/img/tabbar/home_active.svg" alt="" slot="item-icon-active">
         <div slot="item-text">首页</div>
       </TabBarItem>
       <TabBarItem path='/category' activeColor='deepPink'>
-        <img src="~assets/img/tabbar/classification1.svg" alt="" slot="item-icon">
-        <img src="~assets/img/tabbar/classification.svg" alt="" slot="item-icon-active">
+        <img src="~assets/img/tabbar/category.svg" alt="" slot="item-icon">
+        <img src="~assets/img/tabbar/category_active.svg" alt="" slot="item-icon-active">
         <div slot="item-text">分类</div>
       </TabBarItem>
-      <TabBarItem path='cart' activeColor='deepPink'>
-        <img src="~assets/img/tabbar/shopping_cart1.svg" alt="" slot="item-icon">
-        <img src="~assets/img/tabbar/shopping_cart.svg" alt="" slot="item-icon-active">
+      <TabBarItem path='/cart' activeColor='deepPink'>
+        <img src="~assets/img/tabbar/shopcart.svg" alt="" slot="item-icon">
+        <img src="~assets/img/tabbar/shopcart_active.svg" alt="" slot="item-icon-active">
         <div slot="item-text">购物车</div>
       </TabBarItem>
       <TabBarItem path='/profile' activeColor='deepPink'>
-        <img src="~assets/img/tabbar/profile1.svg" alt="" slot="item-icon">
-        <img src="~assets/img/tabbar/profile.svg" alt="" slot="item-icon-active">
+        <img src="~assets/img/tabbar/profile.svg" alt="" slot="item-icon">
+        <img src="~assets/img/tabbar/profile_active.svg" alt="" slot="item-icon-active">
         <div slot="item-text">我的</div>
       </TabBarItem>
     </TabBar>
@@ -26,8 +26,8 @@
 
 <script>
 
-import TabBar from 'components/tabbar/TabBar'
-import TabBarItem from 'components/tabbar/TabBarItem'
+import TabBar from 'components/common/tabbar/TabBar'
+import TabBarItem from 'components/common/tabbar/TabBarItem'
 
 export default {
   name: 'MainTabBar',
